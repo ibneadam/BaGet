b4860d5dd68c796690056a9561f81ffb755651e5
Loic Sharma <sharma.loic@gmail.com>
2018-08-02 21:13:47 -0700

    Add support for Basic Auth

--- src/BaGet/Extensions/AzureAdAuthenticationBuilderExtensions.cs
+++ src/BaGet/Extensions/AzureAdAuthenticationBuilderExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
@@ -33,11 +35,49 @@ namespace BaGet.Extensions
             {
                 options.Audience = _azureOptions.ClientId;
                 options.Authority = $"{_azureOptions.Instance}{_azureOptions.TenantId}";
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        OnMessageReceived(context);
+
+                        return Task.CompletedTask;
+                    }
+                };
             }
 
             public void Configure(JwtBearerOptions options)
             {
                 Configure(Options.DefaultName, options);
+            }
+
+            private void OnMessageReceived(MessageReceivedContext context)
+            {
+                // Try to parse the Basic Authorization crendentials.
+                // See https://github.com/aspnet/Security/blob/beaa2b443d46ef8adaf5c2a89eb475e1893037c2/src/Microsoft.AspNetCore.Authentication.JwtBearer/JwtBearerHandler.cs#L61
+                string authorization = context.HttpContext.Request.Headers["Authorization"];
+
+                if (string.IsNullOrEmpty(authorization))
+                {
+                    return;
+                }
+
+                if (!authorization.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase))
+                {
+                    return;
+                }
+
+                var encodedCredentials = authorization.Substring("Basic ".Length).Trim();
+                var crendentials = Encoding.ASCII.GetString(Convert.FromBase64String(encodedCredentials));
+
+                // The JSON Web Token is the Basic Auth password.
+                if (!crendentials.StartsWith("BaGet:"))
+                {
+                    return;
+                }
+
+                context.Token = crendentials.Substring("BaGet:".Length);
             }
         }
     }
